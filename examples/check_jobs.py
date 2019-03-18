61aa1967f57c6788f2f0964a6a0d46a95ab9155f
Matthew Rahtz <matthew.rahtz@gmail.com>
2019-03-17 18:38:49 -0700

    Add timeout

--- examples/check_jobs.py
+++ examples/check_jobs.py
@@ -21,16 +21,20 @@ import subprocess
 import uuid
 from pathlib import Path
 from socket import gethostname
+from functools import wraps
+import errno
+import os
+import signal
 
 import requests
 
 # AWS SQS URL
-queue_url = "https://.../alerts.fifo"
+queue_url = "https://sqs.us-west-2.amazonaws.com/109526153624/alerts.fifo"
 
 # Log file strings which cause us to cause us to consider a local job broken
-include = ['Exception']
+include = ['exception', 'error', 'Error']
 # But if we see a line like "Exception: HTTPError", then it's OK; don't consider the job broken
-exclude = ['HTTPError']
+exclude = ['ALSA', 'Exception while trying to read metadata', 'INTERNAL SERVER ERROR', 'HTTPError', 'handle_user_exception']
 
 
 def send_alert(text):
@@ -46,6 +50,34 @@ def mark_as_broken(state_path, run_name):
     with open(state_path, 'a') as f:
         f.write(run_name + '\n')
 
+class TimeoutError(Exception):
+    pass
+
+def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
+    def decorator(func):
+        def _handle_timeout(signum, frame):
+            raise TimeoutError(error_message)
+
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+
+        return wraps(func)(wrapper)
+
+    return decorator
+
+def contains_error_lines(log_file):
+    with open(log_file, 'r') as f:
+        lines = f.readlines()
+    for line in lines:
+        if any([p in line for p in include]) and not any([p in line for p in exclude]):
+            return True
+    return False
 
 def check_logs(dirs, state_path, already_broken_runs):
     for run_dir in dirs:
@@ -53,15 +85,13 @@ def check_logs(dirs, state_path, already_broken_runs):
         if run_name in already_broken_runs:
             continue
 
-        error_lines = []
+        broken = False
         for log_file in glob.glob(os.path.join(run_dir, '*.log')):
-            with open(log_file, 'r') as f:
-                lines = f.readlines()
-            for line in lines:
-                if any([p in line for p in include]) and not any([p in line for p in exclude]):
-                    error_lines.append(line)
+            broken = contains_error_lines(log_file)
+            if broken:
+                break
 
-        if error_lines:
+        if broken:
             send_alert(f"Host {gethostname()} run {run_name} broken")
             mark_as_broken(state_path, run_name)
 
@@ -74,7 +104,7 @@ def check_jobs(state_path, already_broken_runs):
         mark_as_broken(state_path, job['jobId'])
         send_alert(f"AWS run {job['jobName']} ({job['jobId']}) failed")
 
-
+@timeout(seconds=10)
 def main():
     parser = argparse.ArgumentParser()
     group = parser.add_argument_group()
@@ -102,4 +132,7 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except TimeoutError:
+        send_alert(f"Timed out while checking logs on {gethostname()}")
